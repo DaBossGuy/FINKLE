2c4142a0d29daa382fa2d2e24f76edb43a77f45e
Sebatian <dabossguymain@gmail.com>
2026-02-01 18:45:16 -0500

    melelele

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ client.on(Events.MessageCreate, async (message) => {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    "model": "cognitivecomputations/dolphin-mistral-24b-venice-edition:free",
+                    "model": "liquid/lfm-2.5-1.2b-instruct:free",
                     "messages": apiMessages // We send the whole conversation history
                 })
             });
